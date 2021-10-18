9344609e6675df7856fd169ef62c58d4507c3645
Jaime <jaimecr72@gmail.com>
2021-10-18 16:42:26 +0000

    first version

---
+++ src/js/component/trafficlight.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+	const [selected, setSelected] = useState("green");
+
+	let classObj = {
+		red: `light red${selected == "red" ? " glow" : ""}`,
+		yellow: `light yellow${selected == "yellow" ? " glow" : ""}`,
+		green: `light green${selected == "green" ? " glow" : ""}`
+	};
+
+	const handleClick = e => {
+		let colorclicked = e.target.classList[1];
+		if (selected !== colorclicked) {
+			classObj[selected] = `light ${selected}`;
+			classObj[colorclicked] = `light ${colorclicked} glow`;
+			setSelected(colorclicked);
+		}
+	};
+
+	return (
+		<div className="container">
+			<div className="traffic-top"></div>
+			<div className="traffic-light">
+				<div className={classObj["red"]} onClick={handleClick}></div>
+				<div className={classObj["yellow"]} onClick={handleClick}></div>
+				<div className={classObj["green"]} onClick={handleClick}></div>
+			</div>
+		</div>
+	);
+};
+
+export default TrafficLight;
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ import "../styles/index.scss";
 
 //import your own components
 import Home from "./component/home.jsx";
+import TrafficLight from "./component/trafficlight.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
